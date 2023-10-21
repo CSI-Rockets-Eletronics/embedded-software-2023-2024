8f50b7962c65d60299fe9ead8855e0dc6ecd0541
Tieqiong Zhang <tz2600@columbia.edu>
2023-10-21 16:13:46 -0400

    analysis

--- flight_computer/analysis.py
+++ flight_computer/analysis.py
@@ -1,5 +1,25 @@
 import serial
 
+class SensorData:
+    acc = list()
+    gyro = list()
+    mag = list()
+
+def parseLine(line: str) -> SensorData:
+    cleaned = line.strip().split(",")
+    print(cleaned)
+    parsed = SensorData()
+    parsed.acc = []
+    parsed.acc[0] = int(cleaned[0].replace("AX: ", ""))
+    parsed.acc[1] = int(cleaned[0].replace("AY: ", ""))
+    parsed.acc[2] = int(cleaned[0].replace("AZ: ", ""))
+    parsed.gyro[0] = int(cleaned[0].replace("GX: ", ""))
+    parsed.gyro[1] = int(cleaned[0].replace("GY: ", ""))
+    parsed.gyro[2] = int(cleaned[0].replace("GZ: ", ""))
+    parsed.mag[0] = int(cleaned[0].replace("MX: ", ""))
+    parsed.mag[1] = int(cleaned[0].replace("MY: ", ""))
+    parsed.mag[2] = int(cleaned[0].replace("MZ: ", ""))
+    return parsed
 
 def readserial(comport, baudrate):
     ser = serial.Serial(
@@ -7,9 +27,8 @@ def readserial(comport, baudrate):
     )  # 1/timeout is the frequency at which the port is read
 
     while True:
-        data = ser.readline().decode().strip()
-        if data:
-            print(data)
+        data = ser.readline().decode()
+        data = parseLine(data)        
+        
 
-
-readserial("/dev/tty.usbserial-10", 115200)
+readserial("COM6", 115200)
