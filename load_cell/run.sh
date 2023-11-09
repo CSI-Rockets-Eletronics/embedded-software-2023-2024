646c95ebe7a2bc58cf8079d9059e80306a39a38e
Alex Chen <axchen7@outlook.com>
2023-11-09 02:55:04 -0500

    Change loadcell upload location

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -3,6 +3,6 @@
 # loop forever
 while true
 do
-    python read_messages.py http://192.168.0.235:3000 0 IDA100 | sudo ./main | python write_records.py http://192.168.0.235:3000 0 IDA100
+    python read_messages.py http://localhost:3000 0 IDA100 | sudo ./main | python write_records.py http://localhost:3000 0 IDA100
     echo "Restarting..."
 done
