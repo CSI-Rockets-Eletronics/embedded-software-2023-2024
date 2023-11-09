591950a614ee9a73bc4065e10ad059335d1e8227
Alex Chen <axchen7@outlook.com>
2023-11-09 04:16:26 -0500

    No loop and do pipefail

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# loop forever
-while true
-do
-    python read_messages.py http://localhost:3000 0 IDA100 | ./main | python write_records.py http://localhost:3000 0 IDA100
-    echo "Restarting..."
-done
+set -eo pipefail
+
+python read_messages.py http://localhost:3000 0 IDA100 | ./main | python write_records.py http://localhost:3000 0 IDA100
