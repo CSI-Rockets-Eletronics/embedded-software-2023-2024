cfab9363a661be79bdf9eb9c24e28d6b99e11345
Alex Chen <axchen7@outlook.com>
2023-12-01 05:44:06 -0500

    Add setup script to install pyserial for serial uploader

---
+++ fs_pi_serial_uploader/setup-pi/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -i
+
+REPO_DIR=$(realpath "$(dirname "$0")/..")
+
+# Go to the repo directory
+cd $REPO_DIR
+
+# Create a python venv and install pyserial
+sudo apt update
+sudo apt install python3-venv
+python -m venv env
+source env/bin/activate
+pip install pyserial
