5608939332820f071a29420649a84cf1a96fc8b5
Alex Chen <axchen7@outlook.com>
2023-12-01 05:48:39 -0500

    Add pyserial dependency to requirements.txt

--- fs_pi_serial_uploader/setup-pi/setup.sh
+++ fs_pi_serial_uploader/setup-pi/setup.sh
@@ -5,9 +5,9 @@ REPO_DIR=$(realpath "$(dirname "$0")/..")
 # Go to the repo directory
 cd $REPO_DIR
 
-# Create a python venv and install pyserial
+# Create a python venv and install the requirements
 sudo apt update
 sudo apt install -y python3-venv
 python -m venv env
 source env/bin/activate
-pip install pyserial
+pip install -r requirements.txt
