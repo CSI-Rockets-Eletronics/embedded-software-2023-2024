302f4e88cb3aab5e3a1a1c755dfbb559149abdc7
Alex Chen <axchen7@outlook.com>
2023-12-21 01:07:00 -0500

    Add MAC address identification to list-mac-addresses.sh

--- pi-esp32-utils/list-mac-addresses.sh
+++ pi-esp32-utils/list-mac-addresses.sh
@@ -1,3 +1,17 @@
+#!/bin/bash -i
+
 for port in /dev/ttyUSB{0..4}; do
-    python -m esptool --port "$port" read_mac | grep MAC | uniq
+    echo "Port: $port"
+    address=$(python -m esptool --port "$port" read_mac | grep MAC | uniq | cut -d ' ' -f 2)
+
+    echo "MAC: $address"
+
+    case $output in
+        "68:b6:b3:3f:07:74")
+            echo "Radio rocket"
+            ;;
+        "68:b6:b3:3f:07:5c")
+            echo "Radio ground"
+            ;;
+    esac
 done
