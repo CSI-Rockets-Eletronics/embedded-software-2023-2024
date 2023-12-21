420b0126de37fb2f8a0af6c26f9b4db27c59a438
Alex Chen <axchen7@outlook.com>
2023-12-21 02:13:38 -0500

    Try to fix paths

--- pi-esp32-utils/flash.sh
+++ pi-esp32-utils/flash.sh
@@ -7,7 +7,7 @@ read -p "Port num (e.g. \"1\"for /dev/ttyUSB1): " port
 
 python3 -m esptool --chip esp32s3 --port "/dev/ttyUSB$port" --baud 921600 \
     --before default_reset --after hard_reset write_flash -z --flash_mode dout --flash_freq 80m --flash_size 4MB \
-    0x0 "~/esp32-build-temp/radio_ga_ga_ground.ino.bootloader.bin" \
-    0x8000 "~/esp32-build-temp/radio_ga_ga_ground.ino.partitions.bin" \
+    0x0 ~/esp32-build-temp/radio_ga_ga_ground.ino.bootloader.bin \
+    0x8000 ~/esp32-build-temp/radio_ga_ga_ground.ino.partitions.bin \
     0xe000 "$SCRIPT_DIR/boot_app0.bin" \
-    0x10000 "~/esp32-build-temp/radio_ga_ga_ground.ino.bin" 
+    0x10000 ~/esp32-build-temp/radio_ga_ga_ground.ino.bin 
