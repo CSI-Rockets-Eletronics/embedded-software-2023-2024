ee696c496644165ad6ffae76f4b0ca8b4aa4ef75
Alex Chen <axchen7@outlook.com>
2024-01-01 02:37:08 -0800

    Implement parse_device for flight computer

--- pi_serial_uploader/main_flight_computer.py
+++ pi_serial_uploader/main_flight_computer.py
@@ -5,17 +5,34 @@ import json
 delimiter = b"\xAA\x55"  # {0b10101010, 0b01010101}
 
 
-def parse(packet: bytes) -> str:
-    if len(packet) != 20:
-        raise ValueError(f"Expected packet length 20, got {len(packet)}")
-
-    # breakdown of "!qhhhhhh":
-    #   "!": network byte order
-    #   "q": long long
-    #   "h": short
-    ts, ax, ay, az, gx, gy, gz = struct.unpack("!qhhhhhh", packet)
-    data = {"ts": ts, "ax": ax, "ay": ay, "az": az, "gx": gx, "gy": gy, "gz": gz}
-    return json.dumps(data)
+def parse_device(packet: bytes) -> str:
+    if len(packet) == 20:
+        return "MPU"
+    if len(packet) == 16:
+        return "DHT"
+    raise ValueError(f"Expected packet length 16 or 20, got {len(packet)}")
 
 
-uploader.run("FlightComputer", delimiter, parse)
+def parse_packet(packet: bytes) -> str:
+    if len(packet) == 20:
+        # breakdown of "!qhhhhhh":
+        #   "!": network byte order
+        #   "q": long long
+        #   "h": short
+        ts, ax, ay, az, gx, gy, gz = struct.unpack("!qhhhhhh", packet)
+        data = {"ts": ts, "ax": ax, "ay": ay, "az": az, "gx": gx, "gy": gy, "gz": gz}
+        return json.dumps(data)
+
+    if len(packet) == 16:
+        # breakdown of "!qff":
+        #   "!": network byte order
+        #   "q": long long
+        #   "f": float
+        ts, temp, hum = struct.unpack("!qff", packet)
+        data = {"ts": ts, "temp": temp, "hum": hum}
+        return json.dumps(data)
+
+    raise ValueError(f"Expected packet length 16 or 20, got {len(packet)}")
+
+
+uploader.run("FlightComputer", delimiter, parse_packet)
