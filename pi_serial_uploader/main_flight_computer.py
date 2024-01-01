89391dd37fcc8d53095841aec31b0266f6250778
Alex Chen <axchen7@outlook.com>
2023-12-31 22:18:33 -0800

    Add packet parsing functionality

--- pi_serial_uploader/main_flight_computer.py
+++ pi_serial_uploader/main_flight_computer.py
@@ -1,3 +1,18 @@
 import uploader
+import struct
+import json
 
-uploader.run("FlightComputer")
+delimiter = b"\xAA\xAA"  # {0b10101010, 0b10101010}
+
+
+def parse(packet: bytes) -> str:
+    # breakdown of "!qhhhhhh":
+    #   "!": network byte order
+    #   "q": long long
+    #   "h": short
+    ts, ax, ay, az, gx, gy, gz = struct.unpack("!qhhhhhh", packet)
+    data = {"ts": ts, "ax": ax, "ay": ay, "az": az, "gx": gx, "gy": gy, "gz": gz}
+    return json.dumps(data)
+
+
+uploader.run("FlightComputer", delimiter, parse)
