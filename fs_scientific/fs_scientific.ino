3c7c8a8ce5784f600c75319e9376480fbe63f762
Alex Chen <axchen7@outlook.com>
2024-01-18 00:18:14 -0500

    Note hardware issue

--- fs_scientific/fs_scientific.ino
+++ fs_scientific/fs_scientific.ino
@@ -40,7 +40,8 @@ const adsGain_t ADC2_GAIN = GAIN_TWOTHIRDS;
 // device constants
 
 const ADCMode OX_TANK_ADC_MODE = ADCMode::Differential_0_1;
-const ADCMode CC_ADC_MODE = ADCMode::Differential_0_1;
+// TODO port for Differential_0_1 has hardware issue?
+const ADCMode CC_ADC_MODE = ADCMode::Differential_2_3;
 
 const float OX_TANK_MPSI_PER_VOLT = 1000;
 const float CC_MPSI_PER_VOLT = 1000;
