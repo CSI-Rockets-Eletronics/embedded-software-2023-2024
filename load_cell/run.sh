96b00d5936e4c924703ef8c06e40cae8a6239b25
Alex Chen <axchen7@outlook.com>
2024-01-20 22:27:10 -0500

    Fix PID variable

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -23,13 +23,13 @@ mkfifo pipe1
     # start main and write_records.py in foreground
     ./main 652964 1750 < pipe0 | python write_records.py http://localhost:3000 0 LoadCell0
     # kill read_messages.py
-    kill $READ_PID
+    kill $READ_PID_0
 ) & (
     set -eo pipefail
     # start read_messages.py in background
-    python read_messages.py http://localhost:3000 0 LoadCell1 > pipe1 & READ_PID=$!
+    python read_messages.py http://localhost:3000 0 LoadCell1 > pipe1 & READ_PID_1=$!
     # start main and write_records.py in foreground
     ./main 1076702 1750 < pipe1 | python write_records.py http://localhost:3000 0 LoadCell1
     # kill read_messages.py
-    kill $READ_PID
+    kill $READ_PID_1
 )
