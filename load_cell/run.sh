8510bcbcad8a37f83e86bed6644b2a9de953538d
Alex Chen <axchen7@outlook.com>
2024-01-20 20:41:08 -0500

    Fix subshells

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -4,5 +4,10 @@ set -eo pipefail
 
 ./unbind.sh
 
-(python read_messages.py http://localhost:3000 0 LoadCell0 | ./main 652964 1750 | python write_records.py http://localhost:3000 0 LoadCell0) \
-& (python read_messages.py http://localhost:3000 0 LoadCell1 | ./main 1076702 1750 | python write_records.py http://localhost:3000 0 LoadCell1)
+(
+    set -eo pipefail
+    python read_messages.py http://localhost:3000 0 LoadCell0 | ./main 652964 1750 | python write_records.py http://localhost:3000 0 LoadCell0
+) & (
+    set -eo pipefail
+    python read_messages.py http://localhost:3000 0 LoadCell1 | ./main 1076702 1750 | python write_records.py http://localhost:3000 0 LoadCell1
+)
