0d919747d6d1a32630df9d2c9bf42fe21b53b326
Alex Chen <axchen7@outlook.com>
2024-01-20 20:36:43 -0500

    Refactor run.sh script to improve process management

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -eo pipefail
+set -e
 
 ./unbind.sh
 
-(python read_messages.py http://localhost:3000 0 LoadCell0 | ./main 652964 1750 | python write_records.py http://localhost:3000 0 LoadCell0) \
-& (python read_messages.py http://localhost:3000 0 LoadCell1 | ./main 1076702 1750 | python write_records.py http://localhost:3000 0 LoadCell1)
+(python read_messages.py http://localhost:3000 0 LoadCell0 && pkill -P $$ || exit 1; ./main 652964 1750 && pkill -P $$ || exit 1; python write_records.py http://localhost:3000 0 LoadCell0) \
+& (python read_messages.py http://localhost:3000 0 LoadCell1 && pkill -P $$ || exit 1; ./main 1076702 1750 && pkill -P $$ || exit 1; python write_records.py http://localhost:3000 0 LoadCell1)
