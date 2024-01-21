740c4074d4ab42f6d8cb1e3f33b6a3e550872dd0
Alex Chen <axchen7@outlook.com>
2024-01-20 20:26:05 -0500

    Update run.sh script to add device identifiers to log messages

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 ./unbind.sh
 
-(python read_messages.py http://localhost:3000 0 LoadCell0 | ./main 652964 1750 | python write_records.py http://localhost:3000 0 LoadCell0) \
-& (python read_messages.py http://localhost:3000 0 LoadCell1 | ./main 1076702 1750 | python write_records.py http://localhost:3000 0 LoadCell1)
+(python read_messages.py http://localhost:3000 0 LoadCell0 | ./main 652964 1750 | python write_records.py http://localhost:3000 0 LoadCell0 | sed 's/^/[device 1] /') \
+& (python read_messages.py http://localhost:3000 0 LoadCell1 | ./main 1076702 1750 | python write_records.py http://localhost:3000 0 LoadCell1 | sed 's/^/[device 2] /')
