7a77d74b46bdc3dcbe514c1bfb26201e9b17e022
Alex Chen <axchen7@outlook.com>
2024-01-20 23:14:20 -0500

    Simplify run.sh

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -4,28 +4,9 @@ set -eo pipefail
 
 ./unbind.sh
 
-# remove pipes if they exist
-if [ -p pipe0 ]; then
-    rm pipe0
-fi
-if [ -p pipe1 ]; then
-    rm pipe1
-fi
-
-# make pipes
-mkfifo pipe0
-mkfifo pipe1
-
 # start read_messages.py in background
-python read_messages.py http://localhost:3000 0 LoadCell0 > pipe0 &
-python read_messages.py http://localhost:3000 0 LoadCell1 > pipe1 &
+python read_messages.py http://localhost:3000 0 LoadCell0 | ./main  652964 1750 < pipe0 | python write_records.py http://localhost:3000 0 LoadCell0 &
+python read_messages.py http://localhost:3000 0 LoadCell1 | ./main 1076702 1750 < pipe1 | python write_records.py http://localhost:3000 0 LoadCell1 &
 
-# start main and write_records.py in background
-./main 652964 1750 < pipe0 | python write_records.py http://localhost:3000 0 LoadCell0 &
-./main 1076702 1750 < pipe1 | python write_records.py http://localhost:3000 0 LoadCell1 &
-
-# wait for any of the background processes to finish
-wait -n
-
-# kill the other background processes
-kill 0
+# wait for all background processes to finish
+wait
