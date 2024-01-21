63ff5bf802bfc559dda11918b588a95cd5a25350
Alex Chen <axchen7@outlook.com>
2024-01-20 22:19:22 -0500

    Tweak script

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -6,8 +6,16 @@ set -eo pipefail
 
 (
     set -eo pipefail
-    python read_messages.py http://localhost:3000 0 LoadCell0 | ./main 652964 1750 | python write_records.py http://localhost:3000 0 LoadCell0
+    mkfifo pipe0
+    python read_messages.py http://localhost:3000 0 LoadCell0 > pipe0 & READ_PID=$!
+    ./main 652964 1750 < pipe0 | python write_records.py http://localhost:3000 0 LoadCell0
+    kill $READ_PID
+    rm pipe0
 ) & (
     set -eo pipefail
-    python read_messages.py http://localhost:3000 0 LoadCell1 | ./main 1076702 1750 | python write_records.py http://localhost:3000 0 LoadCell1
+    mkfifo pipe1
+    python read_messages.py http://localhost:3000 0 LoadCell1 > pipe1 & READ_PID=$!
+    ./main 1076702 1750 < pipe1 | python write_records.py http://localhost:3000 0 LoadCell1
+    kill $READ_PID
+    rm pipe1
 )
