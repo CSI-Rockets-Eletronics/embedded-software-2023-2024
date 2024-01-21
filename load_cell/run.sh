acef5b585d21a9a4ab4c2e229998e74b86e9d7ca
Alex Chen <axchen7@outlook.com>
2024-01-21 00:26:33 -0500

    Use load cell 1, 2

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -23,12 +23,12 @@ mkfifo pipe0
 mkfifo pipe1
 
 # start read_messages.py in background
-python read_messages.py http://localhost:3000 0 LoadCell0 > pipe0 &
-python read_messages.py http://localhost:3000 0 LoadCell1 > pipe1 &
+python read_messages.py http://localhost:3000 0 LoadCell1 > pipe0 &
+python read_messages.py http://localhost:3000 0 LoadCell2 > pipe1 &
 
 # start main and write_records.py in background
-./main 652964 1750 < pipe0 | python write_records.py http://localhost:3000 0 LoadCell0 &
-./main 1076702 1750 < pipe1 | python write_records.py http://localhost:3000 0 LoadCell1 &
+./main 652964 1750 < pipe0 | python write_records.py http://localhost:3000 0 LoadCell1 &
+./main 1076702 1750 < pipe1 | python write_records.py http://localhost:3000 0 LoadCell2 &
 
 # wait for any of the background processes to finish
 wait -n
