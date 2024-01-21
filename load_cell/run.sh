6c5c89a16e51fe533df922af2d2f3abf16adc641
Alex Chen <axchen7@outlook.com>
2024-01-20 23:19:26 -0500

    Remove pipes

--- load_cell/run.sh
+++ load_cell/run.sh
@@ -5,8 +5,8 @@ set -eo pipefail
 ./unbind.sh
 
 # start read_messages.py in background
-python read_messages.py http://localhost:3000 0 LoadCell0 | ./main  652964 1750 < pipe0 | python write_records.py http://localhost:3000 0 LoadCell0 &
-python read_messages.py http://localhost:3000 0 LoadCell1 | ./main 1076702 1750 < pipe1 | python write_records.py http://localhost:3000 0 LoadCell1 &
+python read_messages.py http://localhost:3000 0 LoadCell0 | ./main  652964 1750 | python write_records.py http://localhost:3000 0 LoadCell0 &
+python read_messages.py http://localhost:3000 0 LoadCell1 | ./main 1076702 1750 | python write_records.py http://localhost:3000 0 LoadCell1 &
 
 # wait for all background processes to finish
 wait
