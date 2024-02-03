743e400923b1874a8e28de3b95ac0dc229a04eb0
Alex Chen <axchen7@outlook.com>
2024-02-03 17:15:22 -0500

    Add rocket scientific uploader

---
+++ pi_serial_uploader/main_rocket_scientific.py
@@ -0,0 +1,20 @@
+import uploader
+import struct
+import json
+
+delimiter = b"\xAA\x55"  # {0b10101010, 0b01010101}
+
+
+def parse_packet(packet: bytes) -> str:
+    if len(packet) == 8:
+        # breakdown of "!ii":
+        #   "!": network byte order
+        #   "i": int
+        st1, st2 = struct.unpack("!ii", packet)
+        data = {"st1": st1, "st2": st2}
+        return json.dumps(data)
+
+    raise ValueError(f"Expected packet length 8, got {len(packet)}")
+
+
+uploader.run("RocketScientific", delimiter, parse_packet)
