6fbad1961320be61adfce2f9b0cebc0403fa47fc
Alex Chen <axchen7@outlook.com>
2024-05-11 02:45:13 -0400

    Remove comments

--- radio_ga_ga_ground/radio_ga_ga_ground.ino
+++ radio_ga_ga_ground/radio_ga_ga_ground.ino
@@ -37,12 +37,6 @@ void setup() {
     // with the ishighpowermodule flag set like this:
     rf95.setTxPower(TX_POWER, false);
 
-    // The encryption key has to be the same as the one in the client
-    // uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-    // 				  0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08
-    // 				};
-    // rf95.setEncryptionKey(key);
-
     if (UPLOAD_TO_SERVER) {
         rockets_client::init(rockets_client::wifiConfigPresets.GROUND,
                              rockets_client::serverConfigPresets.MECHE, "0",
@@ -116,25 +110,4 @@ void loop() {
     }
 
     Serial.println();
-
-    // if (rf95.available()) {
-    //     // Should be a message for us now
-    //     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    //     uint8_t len = sizeof(buf);
-    //     if (rf95.recv(buf, &len)) {
-    //         //      RH_RF95::printBuffer("request: ", buf, len);
-    //         Serial.print("got request: ");
-    //         Serial.println((char*)buf);
-    //         //      Serial.print("RSSI: ");
-    //         //      Serial.println(rf95.lastRssi(), DEC);
-
-    //         // Send a reply
-    //         uint8_t data[] = "And hello back to you";
-    //         rf95.send(data, sizeof(data));
-    //         rf95.waitPacketSent();
-    //         Serial.println("Sent a reply");
-    //     } else {
-    //         Serial.println("recv failed");
-    //     }
-    // }
 }
--- radio_ga_ga_rocket/radio_ga_ga_rocket.ino
+++ radio_ga_ga_rocket/radio_ga_ga_rocket.ino
@@ -56,12 +56,6 @@ void setup() {
     // with the ishighpowermodule flag set like this:
     rf95.setTxPower(TX_POWER, false);
 
-    // The encryption key has to be the same as the one in the server
-    // uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-    // 				  0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08
-    // 				};
-    // rf95.setEncryptionKey(key);
-
     rockets_client::init(rockets_client::wifiConfigPresets.ROCKET,
                          rockets_client::serverConfigPresets.ROCKET_PI, "0", "",
                          false, "GPS");
@@ -88,27 +82,4 @@ void loop() {
     rf95.waitPacketSent();
 
     printGpsTsTicker.update();
-
-    // Serial.println("Sending to rf95_server");
-    // // Send a message to rf95_server
-    // uint8_t data[] = "Hello World!";
-    // rf95.send(data, sizeof(data));
-
-    // rf95.waitPacketSent();
-    // // Now wait for a reply
-    // uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    // uint8_t len = sizeof(buf);
-
-    // if (rf95.waitAvailableTimeout(500)) {
-    //     // Should be a reply message for us now
-    //     if (rf95.recv(buf, &len)) {
-    //         Serial.print("got reply: ");
-    //         Serial.println((char*)buf);
-    //     } else {
-    //         Serial.println("recv failed");
-    //     }
-    // } else {
-    //     Serial.println("No reply, is rf95_server running?");
-    // }
-    // delay(400);
 }
