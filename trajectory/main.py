72ab4c9ff889ff1b74edc55dc576648f215ed09a
Alex Chen <axchen7@outlook.com>
2024-06-02 03:11:52 -0700

    Add trajectory service

---
+++ trajectory/main.py
@@ -0,0 +1,247 @@
+from typing import Any, Optional
+import math
+import numpy as np
+import numpy.typing as npt
+from server_utils import (
+    fetch_new_mpu_records,
+    MpuRecordData,
+    TrajectoryRecordData,
+    upload_trajectory_record,
+    has_calibration_message,
+)
+from dataclasses import dataclass
+import quaternion  # type: ignore
+
+
+@dataclass
+class MpuReading:
+    """
+    In SI units:
+        ts: seconds
+        ax,ay,az: m/s^2
+        gx,gy,gz: rad/s
+
+    Axes are relative to the rocket's frame:
+        x: screw switch access side ("front")
+        y: z cross x (observer's right, when facing the screw switches)
+        z: tip of the rocket ("top")
+    """
+
+    ts: float
+    ax: float
+    ay: float
+    az: float
+    gx: float
+    gy: float
+    gz: float
+
+
+def to_mpu_reading(data: MpuRecordData) -> MpuReading:
+    G = 9.81
+    ACC_TICKS_PER_G = 2048
+    GYRO_TICKS_PER_DEG = 16
+    RAD_PER_DEG = math.pi / 180
+
+    # TODO coordinate transformation?
+
+    return MpuReading(
+        ts=data["ts"] / 1e6,
+        ax=data["ax"] * G / ACC_TICKS_PER_G,
+        ay=data["ay"] * G / ACC_TICKS_PER_G,
+        az=data["az"] * G / ACC_TICKS_PER_G,
+        gx=data["gx"] * RAD_PER_DEG / GYRO_TICKS_PER_DEG,
+        gy=data["gy"] * RAD_PER_DEG / GYRO_TICKS_PER_DEG,
+        gz=data["gz"] * RAD_PER_DEG / GYRO_TICKS_PER_DEG,
+    )
+
+
+@dataclass
+class Acceleration:
+    ax: float
+    ay: float
+    az: float
+
+
+def calibrate_stationary_acc() -> Acceleration:
+    STATIONARY_CALIB_SAMPLES = 2000
+
+    calib_samples: list[MpuReading] = []
+
+    while len(calib_samples) < STATIONARY_CALIB_SAMPLES:
+        take = STATIONARY_CALIB_SAMPLES - len(calib_samples)
+        data = fetch_new_mpu_records(take)
+        calib_samples.extend(map(to_mpu_reading, data))
+
+    avg_ax = np.mean([r.ax for r in calib_samples])
+    avg_ay = np.mean([r.ay for r in calib_samples])
+    avg_az = np.mean([r.az for r in calib_samples])
+
+    return Acceleration(
+        ax=float(avg_ax),
+        ay=float(avg_ay),
+        az=float(avg_az),
+    )
+
+
+Quaternion = Any  # quaternion has no type hints :(
+
+
+def rotation_from_stationary_acc(acc: Acceleration) -> Quaternion:
+    """
+    Compute the starting rotation of the rocket. Returns a quaternion
+    representing the rotation from the rocket's internal frame to the world
+    frame.
+
+    We assume the rocket starts with its internal frame aligned with the world
+    frame, e.g. pointing straight upward and rotated such that the internal x
+    axis is aligned with the a well-known direction designated as the world's x
+    axis.
+
+    From this position, we assume the rocket is "tilted" in some direction away
+    from the world's z ("up") axis, but is not rotated around it's internal z
+    axis. The returned quaternion is the unique rotation adhering to the above
+    constraints that explains the measured gravity acceleration vector.
+    """
+
+    # Recall that acc, the gravity acceleration vector, points in the direction
+    # of true "up" in the rocket frame. This is because acc=0 represents free
+    # fall, and being stationary is equivalent to accelerating upward relative
+    # to free fall.
+
+    # Let us represent the rocket's internal "up" vector in the world frame
+    # using spherical coordinates, with theta as the azimuthal angle and phi
+    # as the polar angle (e.g. angle from the world's z axis).
+    anorm = math.sqrt(acc.ax**2 + acc.ay**2 + acc.az**2)
+    phi = math.acos(acc.az / anorm)
+    # the rocket tilting in one direction results in the gravity vector
+    # tilting in the opposite direction (+180 degrees)
+    theta = math.atan2(acc.ay, acc.ax) + math.pi
+
+    # Get the rotation vector representation. We could use Euler angles, but
+    # they're a world of pain (and confusing conventions)...
+
+    # The "tilt" is equivalent to picking the vector pointing along theta + 90
+    # degrees in the xy plane, and rotating about it by phi (per right-hand
+    # rule).
+    rot_vector_theta = theta + math.pi / 2
+    # Recall that a rotation vector encodes the rotation amount in radians
+    # as the vector's magnitude.
+    rot_vector = np.array([math.cos(rot_vector_theta), math.sin(rot_vector_theta), 0])
+    rot_vector *= phi
+
+    # Convert the rotation vector to a quaternion
+    return quaternion.from_rotation_vector(rot_vector)  # type: ignore
+
+
+@dataclass
+class State:
+    """
+    pos: x,y,z position in meters, in the world frame.
+    vel: vx,vy,vz velocity in m/s, in the world frame.
+    acc: ax,ay,az acceleration in m/s^2, in the world frame.
+    rot: rotation quaternion from the rocket frame to the world frame.
+    ts: timestamp in seconds of the last update. None on initialization.
+    """
+
+    pos: npt.NDArray[np.float64]
+    vel: npt.NDArray[np.float64]
+    acc: npt.NDArray[np.float64]
+    rot: Quaternion
+    ts: Optional[float] = None
+
+
+def to_trajectory_record_data(state: State) -> TrajectoryRecordData | None:
+    if state.ts is None:
+        return None
+
+    return TrajectoryRecordData(
+        ts=int(state.ts * 1e6),
+        x=float(state.pos[0]),
+        y=float(state.pos[1]),
+        z=float(state.pos[2]),
+        vx=float(state.vel[0]),
+        vy=float(state.vel[1]),
+        vz=float(state.vel[2]),
+        ax=float(state.acc[0]),
+        ay=float(state.acc[1]),
+        az=float(state.acc[2]),
+    )
+
+
+"""
+Main loop below:
+"""
+
+# assume rocket is pointing straight up at the start (until calibrated)
+initial_rot = quaternion.one()  # type: ignore
+
+state = State(
+    pos=np.array([0, 0, 0]),
+    vel=np.array([0, 0, 0]),
+    acc=np.array([0, 0, 0]),
+    rot=initial_rot,
+)
+
+
+# assume g=9.81 (until calibrated)
+g_magnitude = 9.81  # m/s^2
+
+
+def step(reading: MpuReading) -> None:
+    """Modifies state in-place."""
+
+    # first reading?
+    if state.ts is None:
+        state.ts = reading.ts
+        return
+
+    # compute time step
+    dt = reading.ts - state.ts
+    state.ts = reading.ts
+
+    # compute acceleration in world frame
+    rot_matrix = quaternion.as_rotation_matrix(state.rot)  # type: ignore
+    acc = rot_matrix @ np.array([reading.ax, reading.ay, reading.az])
+    acc[2] -= g_magnitude  # subtract gravity
+
+    # update position and velocity
+    dvel = acc * dt
+    dpos = state.vel * dt + 0.5 * acc * dt**2
+    state.vel += dvel
+    state.pos += dpos
+
+    # update rotation
+    gvec = np.array([reading.gx, reading.gy, reading.gz])
+    theta = np.linalg.norm(gvec) * dt
+    rot_axis = gvec / np.linalg.norm(gvec)
+    quat = quaternion.from_float_array(  # type: ignore
+        [math.cos(theta / 2), *(math.sin(theta / 2) * rot_axis)]
+    )
+    state.rot = (state.rot * quat).normalized()  # type: ignore
+
+
+while True:
+    if has_calibration_message():
+        stat_acc = calibrate_stationary_acc()
+        state = State(
+            pos=np.array([0, 0, 0]),
+            vel=np.array([0, 0, 0]),
+            acc=np.array([0, 0, 0]),
+            rot=rotation_from_stationary_acc(stat_acc),
+        )
+        g_magnitude = math.sqrt(stat_acc.ax**2 + stat_acc.ay**2 + stat_acc.az**2)
+
+    records = fetch_new_mpu_records()
+
+    if len(records) == 0:
+        continue  # to skip uploading
+
+    for entry in records:
+        reading = to_mpu_reading(entry)
+        step(reading)
+
+    # upload just the last state
+    trajectory_data = to_trajectory_record_data(state)
+    if trajectory_data is not None:
+        upload_trajectory_record(trajectory_data)
+        print("Uploaded record:", trajectory_data)
---
+++ trajectory/server_utils.py
@@ -0,0 +1,165 @@
+from typing import TypedDict, Optional, Any, cast
+import requests
+
+url = "http://localhost:3000"
+environment_key = "0"
+
+mpu_device = "MPU"
+trajectory_device = "Trajectory"
+
+calibrate_trajectory_message = "CALIBRATE"
+
+
+class MpuRecordData(TypedDict):
+    ts: int
+    ax: int
+    ay: int
+    az: int
+    gx: int
+    gy: int
+    gz: int
+
+
+class MpuRecord(TypedDict):
+    ts: int
+    data: MpuRecordData
+
+
+class TrajectoryRecordData(TypedDict):
+    ts: int
+    x: float
+    y: float
+    z: float
+    vx: float
+    vy: float
+    vz: float
+    ax: float
+    ay: float
+    az: float
+
+
+class Message(TypedDict):
+    ts: int
+    data: Any
+
+
+def fetch_ts() -> int:
+    """Retries forever"""
+    while True:
+        try:
+            result = requests.get(
+                f"{url}/ts",
+                headers={"content-type": "application/json"},
+            )
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:
+            print(f"Error fetching ts: {e}")
+            continue
+        if result.status_code != 200:
+            print(f"Error fetching ts: status {result.status_code}")
+            continue
+        # success!
+        return result.json()
+
+
+init_ts = fetch_ts()
+
+last_mpu_record_ts = init_ts
+
+
+def fetch_new_mpu_records(take: Optional[int] = None) -> list[MpuRecordData]:
+    """Fetches at most `take` records. Returns an empty list on error"""
+    global last_mpu_record_ts
+
+    params = {
+        "environmentKey": environment_key,
+        "device": mpu_device,
+        "startTs": last_mpu_record_ts + 1,  # startTs is inclusive
+        "take": take,
+    }
+    try:
+        result = requests.get(
+            f"{url}/records",
+            params=params,
+            headers={"content-type": "application/json"},
+        )
+    except KeyboardInterrupt:
+        raise
+    except Exception as e:
+        print(f"Error fetching records: {e}")
+        return []
+    if result.status_code != 200:
+        print(f"Error fetching records: status {result.status_code}")
+        return []
+
+    # success!
+    records: list[MpuRecord] = result.json()
+    if len(records) == 0:
+        return []
+
+    last_mpu_record_ts = records[-1]["ts"]
+    return [record["data"] for record in records]
+
+
+def upload_trajectory_record(data: TrajectoryRecordData) -> bool:
+    """Returns True on success and False on error."""
+    body = {
+        "environmentKey": environment_key,
+        "device": trajectory_device,
+        "data": data,
+    }
+    try:
+        result = requests.post(
+            f"{url}/records",
+            json=body,
+            headers={"content-type": "application/json"},
+        )
+    except KeyboardInterrupt:
+        raise
+    except Exception as e:
+        print(f"Error uploading record: {e}")
+        return False
+    if result.status_code != 200:
+        print(f"Error uploading record: status {result.status_code}")
+        return False
+
+    # success!
+    return True
+
+
+last_trajectory_message_ts = init_ts
+
+
+def has_calibration_message() -> bool:
+    """Returns false on error."""
+    global last_trajectory_message_ts
+
+    params = {
+        "environmentKey": environment_key,
+        "device": mpu_device,
+        "afterTs": last_trajectory_message_ts,
+    }
+    try:
+        result = requests.get(
+            f"{url}/messages/next",
+            params=params,
+            headers={"content-type": "application/json"},
+        )
+    except KeyboardInterrupt:
+        raise
+    except Exception as e:
+        print(f"Error fetching messages: {e}")
+        return False
+    if result.status_code != 200:
+        print(f"Error fetching messages: status {result.status_code}")
+        return False
+
+    # success!
+    message = result.json()
+    if not isinstance(message, dict):
+        return False  # got 'NONE' literal
+
+    message = cast(Message, message)
+    last_trajectory_message_ts = message["ts"]
+    return message["data"] == calibrate_trajectory_message
