5a86a54e386fb59a324022768c5eb0a97678d072
Alex Chen <axchen7@outlook.com>
2024-06-02 20:31:48 -0700

    Add main_rocket.py uploader

--- pi_serial_uploader/main_rocket.py
+++ pi_serial_uploader/main_rocket.py
@@ -6,6 +6,8 @@ delimiter = b"\xAA\x55"  # {0b10101010, 0b01010101}
 
 
 def parse_device(packet: bytes) -> str:
+    if len(packet) == 21:
+        return "RocketScientific"
     if len(packet) == 20:
         return "MPU"
     if len(packet) == 16:
@@ -14,6 +16,16 @@ def parse_device(packet: bytes) -> str:
 
 
 def parse_packet(packet: bytes) -> str:
+    if len(packet) == 21:
+        # breakdown of "!qiiic":
+        #   "!": network byte order
+        #   "q": long long
+        #   "i": int
+        #   "c": char
+        # the last byte is just to make the packet 21 bytes long
+        ts, t1, t2, t3, _c = struct.unpack("!qiii", packet)
+        data = {"ts": ts, "t1": t1, "t2": t2, "t3": t3}
+        return json.dumps(data)
     if len(packet) == 20:
         # breakdown of "!qhhhhhh":
         #   "!": network byte order
@@ -32,7 +44,7 @@ def parse_packet(packet: bytes) -> str:
         data = {"ts": ts, "temp": temp, "hum": hum}
         return json.dumps(data)
 
-    raise ValueError(f"Expected packet length 16 or 20, got {len(packet)}")
+    raise ValueError(f"Expected packet length 16, 20, or 21, got {len(packet)}")
 
 
 uploader.run(parse_device, delimiter, parse_packet)
