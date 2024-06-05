dab6cdd577b6dc7e8bb5c0795193a16973aae93e
Alex Chen <axchen7@outlook.com>
2024-06-04 19:16:04 -0700

    Use GPSSerial #define

--- gps/gps.ino
+++ gps/gps.ino
@@ -47,7 +47,7 @@ void setup() {
 
     Serial.println("Adafruit GPS library basic parsing test!");
 
-    Serial1.setPins(RXPIN, TXPIN);
+    GPSSerial.setPins(RXPIN, TXPIN);
     // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
     GPS.begin(9600);
     // uncomment this line to turn on RMC (recommended minimum) and GGA (fix
