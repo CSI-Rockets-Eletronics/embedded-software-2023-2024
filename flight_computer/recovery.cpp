daca0a09618abbaea6925cbf34f2e93c841bb67f
Alex Chen <axchen7@outlook.com>
2024-06-05 12:14:10 -0700

    Separate timers for dummy chutes

--- flight_computer/recovery.cpp
+++ flight_computer/recovery.cpp
@@ -8,7 +8,8 @@ namespace recovery {
 int DROGUE_CHUTE_PIN = 6;  // transducer output 1
 int MAIN_CHUTE_PIN = 7;    // transducer output 2
 
-int DEPLOY_AFTER_DELAY_MS = 5 * 1000;  // 5 seconds
+int DEPLOY_DROGUE_DELAY_MS = 30 * 1000;  // 30 seconds
+int DEPLOY_MAIN_DELAY_MS = 40 * 1000;    // 40 seconds
 
 void setup() {
     pinMode(DROGUE_CHUTE_PIN, OUTPUT);
@@ -22,11 +23,15 @@ void loop(float acc_total) {
 
     long nowNs = millis();
 
-    if (nowNs > DEPLOY_AFTER_DELAY_MS) {
+    if (nowNs > DEPLOY_DROGUE_DELAY_MS) {
         digitalWrite(DROGUE_CHUTE_PIN, HIGH);
-        digitalWrite(MAIN_CHUTE_PIN, HIGH);
     } else {
         digitalWrite(DROGUE_CHUTE_PIN, LOW);
+    }
+
+    if (nowNs > DEPLOY_MAIN_DELAY_MS) {
+        digitalWrite(MAIN_CHUTE_PIN, HIGH);
+    } else {
         digitalWrite(MAIN_CHUTE_PIN, LOW);
     }
 }
