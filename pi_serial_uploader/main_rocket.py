bd68e728d419d8ca4cb18968a8bffb947b4e3296
Alex Chen <axchen7@outlook.com>
2024-06-04 19:13:21 -0700

    Use adc1 and adc3 to read manifold and ox

--- pi_serial_uploader/main_rocket.py
+++ pi_serial_uploader/main_rocket.py
@@ -6,25 +6,25 @@ delimiter = b"\xAA\x55"  # {0b10101010, 0b01010101}
 
 
 def parse_device(packet: bytes) -> str:
-    if len(packet) == 21:
+    if len(packet) == 17:
         return "RocketScientific"
     if len(packet) == 20:
         return "MPU"
     if len(packet) == 16:
         return "DHT"
-    raise ValueError(f"Expected packet length 16 or 20, got {len(packet)}")
+    raise ValueError(f"Expected packet length 16, 17, or 20, got {len(packet)}")
 
 
 def parse_packet(packet: bytes) -> str:
-    if len(packet) == 21:
-        # breakdown of "!qiiic":
+    if len(packet) == 17:
+        # breakdown of "!qiic":
         #   "!": network byte order
         #   "q": long long
         #   "i": int
         #   "c": char
-        # the last byte is just to make the packet 21 bytes long
-        ts, t1, t2, t3, _c = struct.unpack("!qiiic", packet)
-        data = {"ts": ts, "t1": t1, "t2": t2, "t3": t3}
+        # the last byte is just to make the packet 17 bytes long
+        ts, t1, t3, _c = struct.unpack("!qiic", packet)
+        data = {"ts": ts, "t1": t1, "t3": t3}
         return json.dumps(data)
 
     if len(packet) == 20:
@@ -45,7 +45,7 @@ def parse_packet(packet: bytes) -> str:
         data = {"ts": ts, "temp": temp, "hum": hum}
         return json.dumps(data)
 
-    raise ValueError(f"Expected packet length 16, 20, or 21, got {len(packet)}")
+    raise ValueError(f"Expected packet length 16, 17, or 20, got {len(packet)}")
 
 
 uploader.run(parse_device, delimiter, parse_packet)
--- rocket_scientific/rocket_scientific.ino
+++ rocket_scientific/rocket_scientific.ino
@@ -21,7 +21,6 @@ const int EEPROM_SIZE = 256;
 // written, avoid using 0x00000000 or 0xFFFFFFFF as they may be default values
 const uint32_t EEPROM_WRITTEN_MARKER = 0x12345678;
 const int TRANSD_1_ZERO_EEPROM_ADDR = 4;   // store float
-const int TRANSD_2_ZERO_EEPROM_ADDR = 8;   // store float
 const int TRANSD_3_ZERO_EEPROM_ADDR = 12;  // store float
 
 // I2C constants
@@ -39,40 +38,34 @@ const int ADC_MEDIAN_WINDOW_SIZE = 50;
 
 // specific ADC constants
 
+TwoWire *ADC1_WIRE = &Wire;
+TwoWire *ADC3_WIRE = &Wire1;
+
 const uint8_t ADC1_ADDR = 0x48;
-const uint8_t ADC2_ADDR = 0x49;
 const uint8_t ADC3_ADDR = 0x48;
 
 const uint16_t ADC1_RATE = RATE_ADS1115_860SPS;
-const uint16_t ADC2_RATE = RATE_ADS1115_860SPS;
 const uint16_t ADC3_RATE = RATE_ADS1115_860SPS;
 
 const adsGain_t ADC1_GAIN = GAIN_ONE;
-const adsGain_t ADC2_GAIN = GAIN_ONE;
 const adsGain_t ADC3_GAIN = GAIN_ONE;
 
 // device constants
 
 const ADCMode TRANSD_1_ADC_MODE = ADCMode::SingleEnded_0;
-const ADCMode TRANSD_2_ADC_MODE = ADCMode::SingleEnded_0;
 const ADCMode TRANSD_3_ADC_MODE = ADCMode::SingleEnded_0;
 
 const float TRANSD_1_MPSI_PER_VOLT = 1000 / 0.00341944869;
-const float TRANSD_2_MPSI_PER_VOLT = 1000 / 0.00341944869;
 const float TRANSD_3_MPSI_PER_VOLT = 1000000;
 
 // globals
 
 Adafruit_ADS1115 adc1;
-Adafruit_ADS1115 adc2;
 Adafruit_ADS1115 adc3;
 
 MovingMedianADC<Adafruit_ADS1115> Transd1ADC("transducer 1",
                                              ADC_MEDIAN_WINDOW_SIZE, adc1,
                                              TRANSD_1_ADC_MODE);
-MovingMedianADC<Adafruit_ADS1115> Transd2ADC("transducer 2",
-                                             ADC_MEDIAN_WINDOW_SIZE, adc2,
-                                             TRANSD_2_ADC_MODE);
 MovingMedianADC<Adafruit_ADS1115> Transd3ADC("transducer 3",
                                              ADC_MEDIAN_WINDOW_SIZE, adc3,
                                              TRANSD_3_ADC_MODE);
@@ -117,31 +110,22 @@ void tick() {
 
     // DB should store raw readings, not median
     int32_t t1_host = Transd1ADC.getLatestVolts() * TRANSD_1_MPSI_PER_VOLT;
-    int32_t t2_host = Transd2ADC.getLatestVolts() * TRANSD_2_MPSI_PER_VOLT;
     int32_t t3_host = Transd3ADC.getLatestVolts() * TRANSD_3_MPSI_PER_VOLT;
 
     // deal with endianness
     uint64_t ts = htobe64(ts_host);
     uint32_t t1 = htonl(t1_host);
-    uint32_t t2 = htonl(t2_host);
     uint32_t t3 = htonl(t3_host);
 
     serial.write((uint8_t *)&ts, sizeof(ts));  // 8 bytes
     serial.write((uint8_t *)&t1, sizeof(t1));  // 4 bytes
-    serial.write((uint8_t *)&t2, sizeof(t2));  // 4 bytes
     serial.write((uint8_t *)&t3, sizeof(t3));  // 4 bytes
-
-    // add extra byte to make packet length different from flight computer
-    uint8_t dummyByte = 0x00;
-    serial.write(&dummyByte, 1);  // 1 byte
 
     serial.write(PACKET_DELIMITER, sizeof(PACKET_DELIMITER));
 
     if (PRINT_DEBUG) {
         Serial.print("t1: ");
         Serial.print(t1_host);
-        Serial.print("\tt2: ");
-        Serial.println(t2_host);
         Serial.print("\tt3: ");
         Serial.println(t3_host);
     }
@@ -165,19 +149,16 @@ void readCalibration() {
     uint32_t markerVal = EEPROM.readUInt(0);
     if (markerVal == EEPROM_WRITTEN_MARKER) {
         Transd1ADC.setZero(EEPROM.readFloat(TRANSD_1_ZERO_EEPROM_ADDR));
-        Transd2ADC.setZero(EEPROM.readFloat(TRANSD_2_ZERO_EEPROM_ADDR));
         Transd3ADC.setZero(EEPROM.readFloat(TRANSD_3_ZERO_EEPROM_ADDR));
     }
 }
 
 void recalibrate() {
     float t1Zero = Transd1ADC.recalibrate(ADC_CALIBRATE_SAMPLE_COUNT);
-    float t2Zero = Transd2ADC.recalibrate(ADC_CALIBRATE_SAMPLE_COUNT);
     float t3Zero = Transd3ADC.recalibrate(ADC_CALIBRATE_SAMPLE_COUNT);
 
     EEPROM.writeUInt(0, EEPROM_WRITTEN_MARKER);
     EEPROM.writeFloat(TRANSD_1_ZERO_EEPROM_ADDR, t1Zero);
-    EEPROM.writeFloat(TRANSD_2_ZERO_EEPROM_ADDR, t2Zero);
     EEPROM.writeFloat(TRANSD_3_ZERO_EEPROM_ADDR, t3Zero);
     EEPROM.commit();
 
@@ -186,7 +167,6 @@ void recalibrate() {
 
 void clearCalibration() {
     Transd1ADC.resetZero();
-    Transd2ADC.resetZero();
     Transd3ADC.resetZero();
 
     EEPROM.writeUInt(0, 0x00000000);
@@ -207,27 +187,19 @@ void setup() {
 
     adc1.setDataRate(ADC1_RATE);
     adc1.setGain(ADC1_GAIN);
-    if (!adc1.begin(ADC1_ADDR, &Wire)) {
+    if (!adc1.begin(ADC1_ADDR, ADC1_WIRE)) {
         Serial.println("Failed to start ADC1");
-        while (1);
-    }
-
-    adc2.setDataRate(ADC2_RATE);
-    adc2.setGain(ADC2_GAIN);
-    if (!adc2.begin(ADC2_ADDR, &Wire)) {
-        Serial.println("Failed to start ADC2");
         while (1);
     }
 
     adc3.setDataRate(ADC3_RATE);
     adc3.setGain(ADC3_GAIN);
-    if (!adc3.begin(ADC3_ADDR, &Wire1)) {
+    if (!adc3.begin(ADC3_ADDR, ADC3_WIRE)) {
         Serial.println("Failed to start ADC3");
         while (1);
     }
 
     Transd1ADC.enableContinuous();
-    Transd2ADC.enableContinuous();
     Transd3ADC.enableContinuous();
 
     EEPROM.begin(EEPROM_SIZE);
@@ -243,7 +215,6 @@ void loop() {
 
     // read the ADCs and send sentences to the raspberry pi every tick
     Transd1ADC.tick();
-    Transd2ADC.tick();
     Transd3ADC.tick();
 
     piSerial::tick();
