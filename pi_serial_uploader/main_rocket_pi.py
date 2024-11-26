a97c2d8bbd157b683040c2edfec0e2f86602e051
Alex Chen <axchen7@outlook.com>
2024-11-26 16:56:55 -0500

    Parse many things

--- pi_serial_uploader/main_rocket_pi.py
+++ pi_serial_uploader/main_rocket_pi.py
@@ -5,6 +5,16 @@ import json
 delimiter = b"\xAA\x55"  # {0b10101010, 0b01010101}
 
 
+def parse_device(packet: bytes) -> str:
+    if len(packet) == 23:
+        return "GPS"
+    if len(packet) == 20:
+        return "IMU"
+    if len(packet) == 16:
+        return "DHT"
+    raise ValueError(f"Invalid packet length: {len(packet)}")
+
+
 def parse_packet(packet: bytes) -> str:
     if len(packet) == 23:
         # breakdown of "<QBBBiif":
@@ -18,25 +28,61 @@ def parse_packet(packet: bytes) -> str:
         #   "f": float (4 bytes)
         (
             ts,
-            gps_fix,
-            gps_fixquality,
-            gps_satellites,
-            gps_latitude_fixed,
-            gps_longitude_fixed,
-            gps_altitude,
+            fix,
+            fixquality,
+            satellites,
+            latitude_fixed,
+            longitude_fixed,
+            altitude,
         ) = struct.unpack("<QBBBiif", packet)
         data = {
             "ts": ts,
-            "gps_fix": gps_fix,
-            "gps_fixquality": gps_fixquality,
-            "gps_satellites": gps_satellites,
-            "gps_latitude_fixed": gps_latitude_fixed,
-            "gps_longitude_fixed": gps_longitude_fixed,
-            "gps_altitude": gps_altitude,
+            "fix": fix,
+            "fixquality": fixquality,
+            "satellites": satellites,
+            "latitude_fixed": latitude_fixed,
+            "longitude_fixed": longitude_fixed,
+            "altitude": altitude,
         }
         return json.dumps(data)
 
-    raise ValueError(f"Expected packet length 23, got {len(packet)}")
+    if len(packet) == 20:
+        # breakdown of "<QHHHHHH":
+        #   "<": little-endian
+        #   "Q": uint64_t (8 bytes)
+        #   "H": uint16_t (2 bytes)
+        #   "H": uint16_t (2 bytes)
+        #   "H": uint16_t (2 bytes)
+        #   "H": uint16_t (2 bytes)
+        #   "H": uint16_t (2 bytes)
+        #   "H": uint16_t (2 bytes)
+        ts, ax, ay, az, gx, gy, gz = struct.unpack("<QHHHHHH", packet)
+        data = {
+            "ts": ts,
+            "ax": ax,
+            "ay": ay,
+            "az": az,
+            "gx": gx,
+            "gy": gy,
+            "gz": gz,
+        }
+        return json.dumps(data)
+
+    if len(packet) == 16:
+        # breakdown of "<Qff":
+        #   "<": little-endian
+        #   "Q": uint64_t (8 bytes)
+        #   "f": float (4 bytes)
+        #   "f": float (4 bytes)
+        ts, temperature, humidity = struct.unpack("<Qff", packet)
+        data = {
+            "ts": ts,
+            "temperature": temperature,
+            "humidity": humidity,
+        }
+        return json.dumps(data)
+
+    raise ValueError(f"Invalid packet length: {len(packet)}")
 
 
-uploader.run("Rocket", delimiter, parse_packet)
+uploader.run(parse_device, delimiter, parse_packet)
